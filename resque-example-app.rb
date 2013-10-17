ea1463e7d4f595a7833ffab60ce400a00912068d
wasabhi <abhinav.keswani@trineo.co.nz>
2013-10-18 07:52:44 +1300

    Redis provider agnostic

--- resque-example-app.rb
+++ resque-example-app.rb
@@ -5,10 +5,11 @@ require File.expand_path('../lib/redis_keys', __FILE__)
 require 'sinatra/redis'
 
 configure do
-  uri = URI.parse(ENV["REDISCLOUD_URL"] || ENV["OPENREDIS_URL"] || ENV["REDISGREEN_URL"] || ENV["REDISTOGO_URL"])
+  redis_url = ENV["REDISCLOUD_URL"] || ENV["OPENREDIS_URL"] || ENV["REDISGREEN_URL"] || ENV["REDISTOGO_URL"]
+  uri = URI.parse(redis_url)
   Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
   Resque.redis.namespace = "resque:example"
-  set :redis, ENV["REDISTOGO_URL"]
+  set :redis, redis_url
 end
 
 get "/" do
